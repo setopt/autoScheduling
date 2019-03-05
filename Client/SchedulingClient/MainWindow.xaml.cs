8d97490b349e2ce25ed8816704767e3fd26de1d6
setopt <kirver59@gmail.com>
2019-03-06 00:04:16 +0500

    Добавил клиент с авторизацией

---
+++ Client/SchedulingClient/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SchedulingClient
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtAuthorization_Click(object sender, RoutedEventArgs e)
+        {
+            Service.Service1Client service = new Service.Service1Client();
+            Service.Authentication auth = new Service.Authentication();
+
+            string Login = tbLogin.Text.ToString();
+            string Password = pbPassword.Password.ToString();
+            auth = service.Authentication(Login, Password);
+            if (!auth.error)
+            {
+                if (auth.Role == "admin")
+                {
+                    MenuAdmin menuAdmin = new MenuAdmin();
+                    menuAdmin.Show();
+                    Close();
+                }
+            }
+        }
+    }
+}
